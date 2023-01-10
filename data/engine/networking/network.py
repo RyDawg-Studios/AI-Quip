e144eb59931b1f478f2b1f99c11ed818a5562c93
RyDawgE <59522243+RyDawgE@users.noreply.github.com>
2023-01-10 15:10:29 -0500

    Update network.py

--- data/engine/networking/network.py
+++ data/engine/networking/network.py
@@ -2,20 +2,26 @@ import sys, socket
 import pickle
 
 class Network():
-    def __init__(self, server="", port=5555):
+    def __init__(self, server="", port=5050):
         self.server = server
         self.port = port
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.address = (self.server, self.port)
+        self.connected = False
 
     def connect(self):
         try:
             self.client.connect(self.address)
+            self.connected = True
         except Exception as e:
             print(e)
 
     def disconnect(self):
+        self.connected = False
         self.client.close()
 
     def update(self):
-        print(self.client.recv(8))
+        if self.connected:
+            data = self.client.recv(1024).decode('utf-8')
+            if data:
+                print(data)
